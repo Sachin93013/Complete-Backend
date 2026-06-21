58fe2b2e2e3eec2d8f1a886abc17c6eb1af6c8ee
Sachin <sachinyadav0123321@gmail.com>
2026-06-21 07:57:03 +0530

    update server.js file and create .env file to store the database connection string securely.

--- apis/server.js
+++ apis/server.js
@@ -1,28 +1,49 @@
 import express from "express";
-import mongoose from "mongoose";
+import mongoose from "mongoose";\
+import dotenv from "dotenv";
 
-  (async ()=>{
-     const connectionInstance = await mongoose.connect('mongodb+srv://sachinyadav0123321_db_user:5xGANFkP54hzDbeS@mucluster1.jbyiq9m.mongodb.net/')
-       
-    console.log(connectionInstance.connection.host); 
-  })()
 
-  
+dotenv.config();
+mongoose.connect(process.env.MONGO_URI);
+
+(async () => {
+  const connectionInstance = await mongoose.connect(
+    process.env.MONGO_URI,
+  );
+
+  console.log(connectionInstance.connection.host);
+})();
+
 const app = express();
 
-  app.post("/create-user", (req, res) => {
-    res.send("Hello, World!");
-  })
-
-  // data defination
-  const userSchema = mongoose.Schema({
-    name:String,
-    age:Number
-  })
+app.use(express.json());
 
 
+// data defination
+const userSchema = mongoose.Schema({
+  name: String, 
+  age: Number,
+});
 
+// create collection
+const userCollection = mongoose.model("user", userSchema);
 
-  app.listen(8000, () => {
-    console.log("Server is running on http://localhost:8000");
-  })
+// create api to create user
+app.post("/create-user", async (req, res) => {
+     const userData = req.body
+  const createdUser = await userCollection.create(userData);
+  res.send({
+    "createdUser" : createdUser
+  });
+});
+
+// read api to get all users
+app.get('/get-all-users', async (req, res) => {
+   const users = await userCollection.find()
+   res.send(users)
+})
+
+app.listen(8000, () => {
+  console.log("Server is running on http://localhost:8000");
+});  
+  
